4a3545402c7cdd75df7411d80d7624b803e97b60
Douglas Ashton <douglas.j.ashton@gmail.com>
2016-02-28 21:21:08 +0000

    moved reactive into server.R

--- R Files/server.R
+++ R Files/server.R
@@ -4,7 +4,12 @@ library(ggplot2); library(dplyr); library(tidyr); library(stringr); library(magr
 source('~/GitHub/GCI_Data/R Files/helper.R')
 
 server <- function(input, output){
+
+  cols_radar <- reactive({
+    grep(paste(input$region1, input$region2, sep = "|"), names(fplot))
+  })
+  
   output$MyPlot <- renderChartJSRadar({
-    chartJSRadar(fplot[, c(1, cols_radar)], showToolTipLabel = TRUE)
+    chartJSRadar(fplot[, c(1, cols_radar())], showToolTipLabel = TRUE)
   })
 }
